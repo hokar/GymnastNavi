609ef8f3f5b8d75c4bdecffdfa7740695386724f
hookar <shunsuke886@gmail.com>
2016-02-19 01:40:59 +0900

    プロジェクト作成

---
+++ app/src/androidTest/java/jp/hokari/shunsuke/gymnasticsnavi/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package jp.hokari.shunsuke.gymnasticsnavi
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
